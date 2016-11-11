be9d2665ececfa46fd15592a8d25fb959406f488
Leibnizhu <leibnizhu@gmail.com>
2016-11-11 17:05:31 +0800

    每个WebClient使用数次后关闭重新开启。并在请求头加入随机的来源IP

--- src/main/java/com/turingdi/adpluto/service/URLAccessor.java
+++ src/main/java/com/turingdi/adpluto/service/URLAccessor.java
@@ -9,6 +9,7 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.util.Cookie;
 import com.turingdi.adpluto.entity.GlobalProperties;
+import com.turingdi.adpluto.utils.CommonUtils;
 import com.turingdi.adpluto.utils.Log4jUtils;
 
 public class URLAccessor {
@@ -16,22 +17,34 @@ public class URLAccessor {
     private static final int RETRY_TIMES = 3;
     //连接重试之前的等待时间，秒
     private static final int RETRY_SLEEP = 10;
+    //每个WebClient的使用次数限值
+    private static final int WEBCLIENT_USE_TIMES = 6;
     //当前URLAccessor对象唯一的一个WebClient
     private WebClient webClient;
+    //WebClient使用次数统计
+    private int useCount = 0;
 
     public URLAccessor() {
+        newWebClient();
+    }
+
+    private void newWebClient() {
         //创建一个CHROME浏览器Client
-        webClient = new WebClient(BrowserVersion.CHROME);
+        webClient = new WebClient(BrowserVersion.FIREFOX_38);
         //设置webClient的相关参数
         webClient.setAjaxController(new NicelyResynchronizingAjaxController());
         webClient.getOptions().setJavaScriptEnabled(true);//必须加载JS，保证监控能够被执行
         webClient.getOptions().setCssEnabled(false);//不加载CSS，提高解析速度
         webClient.getOptions().setTimeout(35000);
         webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.addRequestHeader("X-Forwarded-For", CommonUtils.getRandomIPAddr());
+        webClient.addRequestHeader("Proxy-Client-IP", CommonUtils.getRandomIPAddr());
+        webClient.addRequestHeader("WL-Proxy-Client-IP", CommonUtils.getRandomIPAddr());
+        webClient.addRequestHeader("HTTP-Client-IP", CommonUtils.getRandomIPAddr());
     }
 
-
     public boolean accessURL(String clickURL) {
+        checkWebClient();
         Random rand = new Random(System.currentTimeMillis());
         //按指定的比例，从Cookie存储对象中随机获取一个的旧的Cookie进行访问
         webClient.getCookieManager().clearCookies();
@@ -54,6 +67,18 @@ public class URLAccessor {
             }
         }
         return result;
+    }
+
+    /**
+     * 检查WebClient对象使用次数是否到达回收阈值
+     */
+    private void checkWebClient() {
+        useCount++;
+        if(useCount >= WEBCLIENT_USE_TIMES){
+            useCount = 0;
+            webClient.close();
+            newWebClient();
+        }
     }
 
     private boolean tryAccessURL(WebClient webClient, String clickURL) {
