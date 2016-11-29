b4c834ebe4b4f61e520d727c0d40d4a9f1c8c235
Leibnizhu <leibnizhu@gmail.com>
2016-11-29 16:53:25 +0800

    抽取作弊器对象

--- src/main/java/com/turingdi/adpluto/starter/Cheater.java
+++ src/main/java/com/turingdi/adpluto/starter/Cheater.java
@@ -1,8 +1,81 @@
 package com.turingdi.adpluto.starter;
 
-/**
+import com.turingdi.adpluto.entity.CheaterProperty;
+import com.turingdi.adpluto.entity.MissionConfig;
+import com.turingdi.adpluto.entity.RequestParams;
+import com.turingdi.adpluto.utils.Log4jUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/*
  * Created by leibniz on 16-11-29.
  */
-public class Cheater {
+class Cheater {
+    //线程池大小
+    private static final int POOL_SIZE = 5;
+    //线程池对象
+    private ExecutorService threadPool;
+    //刷UV配置
+    private MissionConfig config;
 
+    Cheater(MissionConfig config) {
+        this.config = config;
+    }
+
+    void startCheater() throws InterruptedException {
+        CheaterProperty cheatProps = new CheaterProperty();
+        registerShutdownHook(cheatProps);//注册程序关闭监听钩子
+        initThreadPool(cheatProps, config);//初始化连接池
+        int totalPV = (int) (config.getBasic().getTotaluv() * config.getBasic().getAdvPVAdvUV());
+        int clkCount = 0;//统计当前执行了多少次广告主落地页的PV
+        //遍历所有可能的宏替换排列组合
+        while (clkCount < totalPV) {
+            for (MissionConfig.Campaign camp : config.getCamp()) {
+                for (MissionConfig.Size size : config.getSize()) {
+                    for (String campid : camp.getCampid()) {
+                        for (String crtvPkgId : config.getCtid()) {
+                            for (String adzoneId : config.getSpotid()) {
+                                for (String tag : config.getTag()) {
+                                    for (String clickURL : config.getUrl()) {
+                                        RequestParams req = new RequestParams(clickURL, camp.getAdxid(), size, crtvPkgId, campid, adzoneId, tag);
+                                        //将任务压入任务队列
+                                        while(!cheatProps.getReqQueueStack().offer(req)){
+                                            Thread.sleep(1000);
+                                        }
+                                        //需要触发的点击次数
+                                        if (++clkCount >= totalPV) {
+                                            closeThreadPool(cheatProps);
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void registerShutdownHook(CheaterProperty setting) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> Log4jUtils.getLogger().info("强制退出ing……失败的任务有(显示格式：访问失败任务的URL=失败次数)：\n" + setting.getFailedURL())));
+    }
+
+    private void initThreadPool(CheaterProperty cheatProps, MissionConfig config) {
+        threadPool = Executors.newFixedThreadPool(POOL_SIZE);
+        for(int i = 0; i < POOL_SIZE; i++){
+            threadPool.submit(new CheaterThread(cheatProps, config));
+        }
+    }
+
+    private void closeThreadPool(CheaterProperty setting) throws InterruptedException {
+        threadPool.shutdown();
+        while(!threadPool.awaitTermination(10, TimeUnit.SECONDS)){
+            Log4jUtils.getLogger().info("等待线程池关闭...");
+        }
+        Log4jUtils.getLogger().info("失败的任务有：" + setting.getFailedURL());
+        Log4jUtils.getLogger().info("线程池已关闭，正在退出...");
+    }
 }
--- src/main/java/com/turingdi/adpluto/starter/MainStarter.java
+++ src/main/java/com/turingdi/adpluto/starter/MainStarter.java
@@ -1,27 +1,17 @@
 package com.turingdi.adpluto.starter;
 
 import com.turingdi.adpluto.entity.MissionConfig;
-import com.turingdi.adpluto.entity.MissionConfig.Size;
-import com.turingdi.adpluto.entity.RequestParams;
-import com.turingdi.adpluto.entity.CheaterSetting;
 import com.turingdi.adpluto.entity.SystemConfig;
 import com.turingdi.adpluto.rpc.NettyRPCServer;
 import com.turingdi.adpluto.service.ProxyHolder;
 import com.turingdi.adpluto.utils.Log4jUtils;
 import com.turingdi.adpluto.utils.MySQLUtils;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class MainStarter {
-    //线程池大小
-    private static final int POOL_SIZE = 5;
-    //线程池对象
-    private ExecutorService threadPool;
+
 
     public static void main(String[] args) {
-        basicInit();
+        commonInit();
         try {
             if (args.length > 0 && "-server".equals(args[0])) {
                 runCheatServer();
@@ -31,9 +21,14 @@ public class MainStarter {
         } catch (Exception e) {
             Log4jUtils.getLogger().error("启动服务时抛出异常", e);
         }
+        commonClose();
     }
 
-    private static void basicInit() {
+    private static void commonClose() {
+        MySQLUtils.closePool();//关闭MySQL连接池
+    }
+
+    private static void commonInit() {
         Log4jUtils.getLogger().info("已读取系统配置文件sys.json：" + SystemConfig.getInstance());//初始化系统基本配置
         MySQLUtils.initMySQLPool();//初始化MySQL连接池
         ProxyHolder.getInstance().getClass();//初始化本地IP代理池
@@ -41,71 +36,13 @@ public class MainStarter {
 
     private static void runCheatOnce() throws InterruptedException {
         Log4jUtils.getLogger().info("单次执行模式");
-        Log4jUtils.getLogger().info("已读取任务配置文件config.json：" + MissionConfig.getGlobalProps());//初始化作弊基本配置
-        MissionConfig.getGlobalProps().shuffle();//打乱配置
-        MainStarter starter = new MainStarter();
-        starter.startCheater();//启动作弊器
-        MySQLUtils.closePool();//关闭MySQL连接池
+        MissionConfig jsonConfig= MissionConfig.parseFromDefaultFile();
+        Log4jUtils.getLogger().info("已读取任务配置文件config.json并打乱：" + jsonConfig);//初始化作弊基本配置
+        new Cheater(jsonConfig).startCheater();//启动作弊器
     }
 
     private static void runCheatServer() throws Exception {
         Log4jUtils.getLogger().info("RPC服务器模式");
         new NettyRPCServer().start();
-    }
-
-    private void startCheater() throws InterruptedException {
-        CheaterSetting setting = new CheaterSetting();
-        registerShutdownHook(setting);//注册程序关闭监听钩子
-        initThreadPool(setting);//初始化连接池
-        MissionConfig props = MissionConfig.getGlobalProps();
-        int totalPV = (int) (props.getBasic().getTotaluv() * props.getBasic().getAdvPVAdvUV());
-        int clkCount = 0;//统计当前执行了多少次广告主落地页的PV
-        //遍历所有可能的宏替换排列组合
-        while (clkCount < totalPV) {
-            for (MissionConfig.Campaign camp : props.getCamp()) {
-                for (Size size : props.getSize()) {
-                    for (String campid : camp.getCampid()) {
-                        for (String crtvPkgId : props.getCtid()) {
-                            for (String adzoneId : props.getSpotid()) {
-                                for (String tag : props.getTag()) {
-                                    for (String clickURL : props.getUrl()) {
-                                        RequestParams req = new RequestParams(clickURL, camp.getAdxid(), size, crtvPkgId, campid, adzoneId, tag);
-                                        //将任务压入任务队列
-                                        while(!setting.getReqQueueStack().offer(req)){
-                                            Thread.sleep(1000);
-                                        }
-                                        //需要触发的点击次数
-                                        if (++clkCount >= totalPV) {
-                                            closeThreadPool(setting);
-                                            return;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void registerShutdownHook(CheaterSetting setting) {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> Log4jUtils.getLogger().info("强制退出ing……失败的任务有(显示格式：访问失败任务的URL=失败次数)：\n" + setting.getFailedURL())));
-    }
-
-    private void initThreadPool(CheaterSetting setting) {
-        threadPool = Executors.newFixedThreadPool(POOL_SIZE);
-        for(int i = 0; i < POOL_SIZE; i++){
-            threadPool.submit(new CheaterThread(setting));
-        }
-    }
-
-    private void closeThreadPool(CheaterSetting setting) throws InterruptedException {
-        threadPool.shutdown();
-        while(!threadPool.awaitTermination(10, TimeUnit.SECONDS)){
-            Log4jUtils.getLogger().info("等待线程池关闭...");
-        }
-        Log4jUtils.getLogger().info("失败的任务有：" + setting.getFailedURL());
-        Log4jUtils.getLogger().info("线程池已关闭，正在退出...");
     }
 }
