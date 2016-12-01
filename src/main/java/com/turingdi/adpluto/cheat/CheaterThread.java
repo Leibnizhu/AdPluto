0dc781bc21db1ac35e0233c6e820ce4ab8907571
Leibniz.Hu <leibnizhu@gmail.com>
2016-12-01 13:56:43 +0800

    更改连接池的使用方式，每个线程执行一个访问任务，取消任务队列

--- src/main/java/com/turingdi/adpluto/cheat/CheaterThread.java
+++ src/main/java/com/turingdi/adpluto/cheat/CheaterThread.java
@@ -18,54 +18,43 @@ public class CheaterThread implements Callable<String> {
     private URLAccessor urlAccessor;
     private CheaterProperty cheatProps;
     private MissionConfig missionConfig;
+    private RequestParams req;
 
-    CheaterThread(CheaterProperty cheatProps, MissionConfig missionConfig) {
-        this.cheatProps = cheatProps;
+    CheaterThread(Cheater cheater, MissionConfig missionConfig, RequestParams req) {
+        this.cheatProps = cheater.getCheatProps();
         this.missionConfig = missionConfig;
+        this.req = req;
         urlAccessor = new URLAccessor(missionConfig);
     }
 
     @Override
     public String call() throws Exception {
-        LinkedBlockingQueue<RequestParams> reqQueueStack = cheatProps.getReqQueueStack();
         Map<String, Integer> failedURL = cheatProps.getFailedURL();
-        Thread.sleep(5000);
-        while (true) {
-            // 从队列弹出数据
-            Log4jUtils.getLogger().info("任务队列长度：" + reqQueueStack.size());
-            if (reqQueueStack.size() > 0) {
-                try {
-                    RequestParams req = reqQueueStack.poll();
-                    cheatProps.addBrowsedCount();
-                    int curCount = cheatProps.getBrowsedCount();
-                    Log4jUtils.getLogger().info("执行第" + curCount + "次任务");
-                    String clickURL = req.getClickURL();
-                    Log4jUtils.getLogger().info("实际访问URL：" + clickURL);
-                    //按指定的PVUV比例访问URL
-                    if (urlAccessor.accessURL(clickURL)) {
-                        if (req.getMysqlAdxId() != null) {
-                            //写入扒数平台的MySQL
-                            DatabaseAccessor.getInstance().incrDataBase(req, missionConfig);
-                        } else {
-                            Log4jUtils.getLogger().info("本次访问无需写入Data Optimus数据库");
-                        }
-                        Log4jUtils.getLogger().info("============>第" + curCount + "次任务已完成<============");
-                    } else {
-                        Integer failTime = failedURL.get(clickURL);
-                        failTime = null == failTime ? 0 : failTime;
-                        failedURL.put(clickURL, failTime + 1);
-                        Log4jUtils.getLogger().info("============>第" + curCount + "次任务失败<============");
-                    }
-                } catch (Exception ignored) {
+        try {
+            cheatProps.addBrowsedCount();
+            int curCount = cheatProps.getBrowsedCount();
+            Log4jUtils.getLogger().info("执行第" + curCount + "次任务");
+            String clickURL = req.getClickURL();
+            Log4jUtils.getLogger().info("实际访问URL：" + clickURL);
+            //按指定的PVUV比例访问URL
+            if (urlAccessor.accessURL(clickURL)) {
+                if (req.getMysqlAdxId() != null) {
+                    //写入扒数平台的MySQL
+                    DatabaseAccessor.getInstance().incrDataBase(req, missionConfig);
+                } else {
+                    Log4jUtils.getLogger().info("本次访问无需写入Data Optimus数据库");
                 }
+                Log4jUtils.getLogger().info("============>第" + curCount + "次任务已完成<============");
             } else {
-                Thread.sleep(10000);
-                if (reqQueueStack.size() == 0) {
-                    break;
-                }
+                Integer failTime = failedURL.get(clickURL);
+                failTime = null == failTime ? 0 : failTime;
+                failedURL.put(clickURL, failTime + 1);
+                Log4jUtils.getLogger().info("============>第" + curCount + "次任务失败<============");
             }
+            urlAccessor.close();
+        } catch (Exception ignored) {
         }
-        Log4jUtils.getLogger().info("线程" + Thread.currentThread() + "处理完毕，准备关闭...");
+        Log4jUtils.getLogger().info("线程" + Thread.currentThread() + "处理完毕，即将关闭...");
         return "Success";
     }
 }
--- src/main/java/com/turingdi/adpluto/entity/CheaterProperty.java
+++ src/main/java/com/turingdi/adpluto/entity/CheaterProperty.java
@@ -8,18 +8,10 @@ import java.util.concurrent.LinkedBlockingQueue;
  * Created by leibniz on 16-11-29.
  */
 public class CheaterProperty {
-    //任务队列最大长度
-    private static final int QUEUE_MAX_LENGTH = 100;
-    //任务队列
-    private final LinkedBlockingQueue<RequestParams> reqQueueStack = new LinkedBlockingQueue<>(QUEUE_MAX_LENGTH);
     //任务执行次数统计
     private volatile Integer browsedCount = 0;
     //失败任务统计
     private final Map<String, Integer> failedURL = new HashMap<>();
-
-    public LinkedBlockingQueue<RequestParams> getReqQueueStack() {
-        return reqQueueStack;
-    }
 
     public Integer getBrowsedCount() {
         return browsedCount;
@@ -29,7 +21,7 @@ public class CheaterProperty {
         return failedURL;
     }
 
-    public void addBrowsedCount(){
+    public synchronized void addBrowsedCount(){
         browsedCount++;
     }
 }
