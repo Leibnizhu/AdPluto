d133b805ee57e62ed8c71d487311fc32b8981357
Leibniz.Hu <leibnizhu@gmail.com>
2016-12-02 13:45:18 +0800

    Proxy对象继承ProxyConfig，增加对应构造方法，并增加HashCode和Equals方法以便HashMap使用

--- src/main/java/com/turingdi/adpluto/entity/Proxy.java
+++ src/main/java/com/turingdi/adpluto/entity/Proxy.java
@@ -1,13 +1,28 @@
 package com.turingdi.adpluto.entity;
 
+import com.gargoylesoftware.htmlunit.ProxyConfig;
+
 /*
  * Created by leibniz on 16-11-28.
  */
 @SuppressWarnings("unused")
-public class Proxy {
+public class Proxy extends ProxyConfig{
+    //用于从Json中解析
     private String ip;
     private int port;
     private String area;
+
+    public Proxy(ProxyConfig proxy) {
+        super(proxy.getProxyHost(), proxy.getProxyPort(), proxy.isSocksProxy());
+        this.ip = proxy.getProxyHost();
+        this.port = proxy.getProxyPort();
+    }
+
+    public Proxy initSuper(){
+        super.setProxyHost(ip);
+        super.setProxyPort(port);
+        return this;
+    }
 
     public Proxy() {
     }
@@ -34,5 +49,20 @@ public class Proxy {
 
     public void setPort(int port) {
         this.port = port;
+    }
+
+    @Override
+    public int hashCode() {
+        return (super.getProxyHost()+":"+super.getProxyPort()).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof ProxyConfig))
+            return false;
+        ProxyConfig proxy = (ProxyConfig)obj;
+        return super.getProxyHost().equals(proxy.getProxyHost())
+                && super.getProxyPort() == proxy.getProxyPort()
+                && super.isSocksProxy() == proxy.isSocksProxy();
     }
 }
